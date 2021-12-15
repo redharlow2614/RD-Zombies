4291885059a0812d6966d1a8320f61d0eebc35d1
redharlow2614 <71796038+redharlow2614@users.noreply.github.com>
2021-12-15 09:33:47 -0700

    fixed?

--- client/gameplay/nopeds.lua
+++ client/gameplay/nopeds.lua
@@ -1,5 +1,6 @@
 Citizen.CreateThread(function()
 	while true do
+		Wait(1000)
 		Citizen.InvokeNative(0xBA0980B5C0A11924, 0)
 		Citizen.InvokeNative(0xFEDFA97638D61D4A, 0)
 		Citizen.InvokeNative(0x1F91D44490E1EA0C, 0)
