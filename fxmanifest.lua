1c68923dcc516ce0390cba7157c71d80f0242d31
redharlow2614 <71796038+redharlow2614@users.noreply.github.com>
2021-12-15 06:52:58 -0700

    removed unused and simplified code

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -2,12 +2,7 @@ fx_version 'cerulean'
 games { 'rdr3' }
 rdr3_warning 'I acknowledge that this is a prerelease build of RedM, and I am aware my resources *will* become incompatible once RedM ships.'
 shared_script {"shared/useful/functions.lua"}
-server_scripts {
-	-- "server/main.lua",
-	-- "server/**/*.lua"
-}
 
 client_scripts {
-	"client/spawners/zombiespawner.lua",
-	"client/useful/util.lua"
+	"client/**/*.lua"
 }
