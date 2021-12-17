f6d0d078e341ecdf691e409a73c6c2cdb50b9680
redharlow2614 <71796038+redharlow2614@users.noreply.github.com>
2021-12-17 12:37:01 -0700

    Added Config Finally

--- config/config.lua
+++ config/config.lua
@@ -1,1 +1,10 @@
-#SOON
+_RDConfig = {
+  MaxSpawnDistance = 50,
+  ShowBlips = true,
+  BlipSprite = -839369609,
+  SafeZones = {
+    ['Valentine'] = {
+      x = -328.82, y = 773.82, z = 117.49, radius = 100.0
+    },
+  }
+}
